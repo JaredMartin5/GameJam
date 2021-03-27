f250495c33d8919e97b04eb7154b293e843d932a
jaredmartin5 <jaredm776@cox.net>
2021-03-27 18:34:38 -0500

    need to pull

--- Assets/damage.cs
+++ Assets/damage.cs
@@ -16,7 +16,7 @@ public class damage : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.name == "CratePink")
+        if (col.gameObject.name == "CratePink")
         {
             health -= 5;
             print(health);
@@ -37,7 +37,7 @@ public class damage : MonoBehaviour
                 }
                 Destroy(col.gameObject);
 
-            }
+            } 
 
 
             print(health);
